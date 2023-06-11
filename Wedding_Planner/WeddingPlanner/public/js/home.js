28808ee1c1d088b7bd85332fe92b0c3fa4036df1
ichhrak <hatimiichrak@gmail.com>
2023-06-10 21:25:23 -0400

    second

---
+++ Wedding_Planner/WeddingPlanner/public/js/home.js
@@ -0,0 +1,12 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+
+productContainers.forEach((item, i) => {
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
